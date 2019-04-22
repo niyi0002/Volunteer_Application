65d0969f0cbfb37be85eb96ddf9f33b842b25064
BamlakHune <bamlakworku12@gmail.com>
2019-04-22 13:29:58 +0200

    About us is done

--- src/Controller/AboutUs.java
+++ src/Controller/AboutUs.java
@@ -1,4 +1,39 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class AboutUs {
+
+    @FXML
+    private Label labelAboutUs;
+    @FXML
+    private Text textAboutUs;
+
+    @FXML
+    private Button mainButton;
+    Stage dialogStage = new Stage();
+    Scene scene;
+
+    @FXML
+    private void handleMainButton(ActionEvent event){
+        try {
+            Node node = (Node) event.getSource();
+            dialogStage = (Stage) node.getScene().getWindow();
+            dialogStage.close();
+            scene = new Scene(FXMLLoader.load(getClass().getResource("../View/DefaultPage.fxml")));
+            dialogStage.setScene(scene);
+            dialogStage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
