12c3a17a5fe7e5b856420f506d01930f075e4b22
ayse <ayse_775@hotmail.com>
2019-04-24 14:36:21 +0200

    + SIGN in added

--- src/Controller/SignIn.java
+++ src/Controller/SignIn.java
@@ -1,4 +1,79 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import sample.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class SignIn {
+
+    @FXML
+    private TextField userName;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button signInButton;
+    @FXML
+    private Button returnButton;
+
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    Stage dialogStage = new Stage();
+    Scene scene;
+
+    public SignIn(){
+        connection = DatabaseConnection.dbConnect();
+
+    }
+
+    @FXML private void handleSignIn(ActionEvent event){
+        String userName1 = userName.getText().toString();
+        String password2 = password.getText().toString();
+
+        String sql = "SELECT * FROM adminInfo WHERE username = ? and password = ?";
+
+        try{
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, userName1);
+            preparedStatement.setString(2, password2);
+            resultSet = preparedStatement.executeQuery();
+            if(!resultSet.next()){
+
+                Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setHeaderText("Input invalid");
+                errorAlert.setContentText("Username or password is incorrect");
+                errorAlert.showAndWait();
+                System.out.println("Failed");
+
+            }else{
+                System.out.println("Succesful");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        /*@FXML private void handleReturnButton(ActionEvent event){
+           Node node = (Node)event.getSource();
+                dialogStage = (Stage) node.getScene().getWindow();
+                dialogStage.close();
+                scene = new Scene(FXMLLoader.load(getClass().getResource("menu.fxml")));
+                dialogStage.setScene(scene);
+                dialogStage.show();*/
+
+    }
+
+
 }
+
