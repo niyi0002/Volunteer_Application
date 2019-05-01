0c6ec413a2013ea683599046cf7860583b3eda18
PetarPetrovic98 <38016771+PetarPetrovic98@users.noreply.github.com>
2019-05-01 11:45:12 +0200

    Changed Admin, Volunteer, DefaultPage.fxml

--- src/Model/Admin.java
+++ src/Model/Admin.java
@@ -1,4 +1,4 @@
 package Model;
 
-public class Admin {
+public class Admin extends User{
 }
--- src/Model/Volunteer.java
+++ src/Model/Volunteer.java
@@ -1,4 +1,36 @@
 package Model;
 
-public class Volunteer {
+public class Volunteer extends User{
+
+
+    public String birthday;
+    public String phoneNbr;
+    public String address;
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhoneNbr() {
+        return phoneNbr;
+    }
+
+    public void setPhoneNbr(String phoneNbr) {
+        this.phoneNbr = phoneNbr;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
 }
