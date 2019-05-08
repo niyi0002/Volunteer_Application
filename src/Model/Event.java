9796e171a148d18cbbefa48ed8427b4e508edf48
ayse <ayse_775@hotmail.com>
2019-05-08 15:04:03 +0200

    + property-update in event

--- src/Model/Event.java
+++ src/Model/Event.java
@@ -1,82 +1,133 @@
 package Model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 import java.sql.Date;
 
 public class Event {
 
-    private int eventID;
-    private String eventName;
-    private String eventDate;
-    private String eventTime;
-    private String eventInfo;
-    private String eventOrganizer;
-    private String country;
-    private String city;
+    private IntegerProperty eventID = new SimpleIntegerProperty(this, "eventID");
+    private StringProperty eventName = new SimpleStringProperty(this, "eventName");
+    private StringProperty eventDate = new SimpleStringProperty(this, "eventDate");
+    private StringProperty eventTime = new SimpleStringProperty(this, "eventTime");
+    private StringProperty eventInfo = new SimpleStringProperty(this, "eventInfo");
+    private StringProperty eventOrganizer = new SimpleStringProperty(this, "eventOrganizer");
+    private StringProperty country = new SimpleStringProperty(this, "country");
+    private StringProperty city = new SimpleStringProperty(this, "city");
 
 
-    public Event(){
+    public Event(IntegerProperty eventID, StringProperty eventName, StringProperty eventDate, StringProperty eventTime
+            , StringProperty eventInfo, StringProperty eventOrganizer, StringProperty country, StringProperty city) {
+        this.eventID = eventID;
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
+        this.eventInfo = eventInfo;
+        this.eventOrganizer = eventOrganizer;
+        this.country = country;
+        this.city = city;
 
     }
 
-    public void setEventID(int eventID) {
-        this.eventID = eventID;
+    public Event() {
     }
 
     public void setEventName(String eventName) {
-        this.eventName = eventName;
+        this.eventName.set(eventName);
     }
 
-    public void setEventDate(Date eventDate){
-        this.eventDate = String.valueOf(eventDate);
+    public void setEventID(int eventID) {
+        this.eventID.set(eventID);
     }
 
-    public void setEventTime(String eventTime){
-        this.eventTime = eventTime;
+    public void setEventDate(Date eventDate) {
+        this.eventDate.set(String.valueOf(eventDate));
+    }
+
+    public void setEventTime(String eventTime) {
+        this.eventTime.set(eventTime);
     }
 
     public void setEventInfo(String eventInfo) {
-        this.eventInfo = eventInfo;
-    }
-
-    public void setCountry(String country){
-        this.country = country;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
+        this.eventInfo.set(eventInfo);
     }
 
     public void setEventOrganizer(String eventOrganizer) {
-        this.eventOrganizer = eventOrganizer;
+        this.eventOrganizer.set(eventOrganizer);
     }
 
+    public void setCountry(String country) {
+        this.country.set(country);
+    }
+
+    public void setCity(String city) {
+        this.city.set(city);
+    }
 
     public int getEventID() {
-        return eventID;
+        return eventID.get();
     }
 
     public String getEventName() {
-        return eventName;
+        return eventName.get();
     }
 
     public String getEventDate() {
-        return eventDate;
+        return eventDate.get();
     }
 
     public String getEventTime() {
-        return eventTime;
+        return eventTime.get();
     }
 
-    public String getEventInfo() { return eventInfo; }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getCity() { return city;
+    public String getEventInfo() {
+        return eventInfo.get();
     }
 
     public String getEventOrganizer() {
+        return eventOrganizer.get();
+    }
+
+    public String getCountry() {
+        return country.get();
+    }
+
+    public String getCity() {
+        return city.get();
+    }
+
+    public IntegerProperty eventIDProperty() {
+        return eventID;
+    }
+
+    public StringProperty eventNameProperty() {
+        return eventName;
+    }
+
+    public StringProperty eventDateProperty() {
+        return eventDate;
+    }
+
+    public StringProperty eventTimeProperty() {
+        return eventTime;
+    }
+
+    public StringProperty eventInfoProperty() {
+        return eventInfo;
+    }
+
+    public StringProperty eventOrganizerProperty() {
         return eventOrganizer;
+    }
+
+    public StringProperty countryProperty() {
+        return country;
+    }
+
+    public StringProperty cityProperty() {
+        return city;
     }
 }
