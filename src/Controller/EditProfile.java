126c1222deade275a311df3f27d7cd210e2f5919
ayse <ayse_775@hotmail.com>
2019-05-19 18:25:12 +0200

    + edit profile, alert boxes

--- src/Controller/EditProfile.java
+++ src/Controller/EditProfile.java
@@ -4,6 +4,7 @@ import Model.Volunteer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import sample.ChangeScene;
@@ -50,31 +51,55 @@ public class EditProfile implements Initializable {
 
 
     @FXML
-    void handleUpdateButton(ActionEvent event) {
+    void handleUpdateButton(ActionEvent event) throws SQLException {
+
+
+        String SSN = "[0-9]{8}-[0-9]{4}";
+        String NAME = "[a-zA-Z]";
+        String PHONE = "[0-9]{10}";
 
         DatabaseConnection databaseConnection = new DatabaseConnection();
         Volunteer volunteer = new Volunteer();
         String user = SignIn.getCurrentUser();
 
-        if (!firstNameField.getText().equals("")) {
+
+        if (!firstNameField.getText().equals(databaseConnection.getFirstName(user)) && firstNameField.getText().matches(NAME)) {
             volunteer.setFirstname(firstNameField.getText());
             databaseConnection.updateFirstName(user, volunteer);
-        }
+        } else {alertBox1();}
 
-        if (!lastNameField.getText().equals("")) {
+        if (!lastNameField.getText().equals(databaseConnection.getLastName(user)) && lastNameField.getText().matches(NAME)) {
             volunteer.setLastname(lastNameField.getText());
             databaseConnection.updateLastName(user, volunteer);
-        }
+        } else {alertBox1();}
 
-        if (!addressField.getText().equals("")) {
+        if (!addressField.getText().equals(databaseConnection.getPhoneNbr(user))) {
             volunteer.setAddress(addressField.getText());
             databaseConnection.updateAddress(user, volunteer);
         }
 
-        if (!phoneNbrField.getText().equals("")) {
+        if (!phoneNbrField.getText().equals(databaseConnection.getPhoneNbr(user)) && phoneNbrField.getText().matches(PHONE)) {
             volunteer.setPhoneNbr(phoneNbrField.getText());
             databaseConnection.updatePhoneNbr(user, volunteer);
-        }
+        }else{alertBox2();}
+
+    }
+
+    private void alertBox1() {
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setHeaderText("Input invalid");
+        errorAlert.setContentText("First name/last name can not contain digits.");
+        errorAlert.showAndWait();
+        System.out.println("Failed");
+
+    }
+
+    private void alertBox2() {
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setHeaderText("Input invalid");
+        errorAlert.setContentText("Phone number must only contain 10 digits.");
+        errorAlert.showAndWait();
+        System.out.println("Failed");
 
     }
 
